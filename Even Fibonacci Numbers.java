4b159e7dcfe8e7585073af6c5930896c1330006e
Rohini Kar <rohinikar@users.noreply.github.com>
2016-05-20 14:43:45 -0700

    Added code for two problems

---
+++ Even Fibonacci Numbers.java
@@ -0,0 +1,22 @@
+public static void main(String[] args)
+{
+    int res = fibonacci(1,2);
+	System.out.println("The sum of even Fibonacci numbers less than 4 million = %d", res)
+}
+
+int fibonacci(int i1, int i2)
+{
+    int result = 0;
+	result += i2;
+	    int sum = i1 + i2;
+	while (sum <= 4000000)
+	{
+	    i1 = i2;
+		i2 = sum;
+		sum = i1 + i2;
+		
+		if(sum % 2 == 0)
+		    result += sum;
+	}
+	return result;
+}
