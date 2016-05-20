fc342bffad0b98dd38e2f44cd47cb7ab8bf4bd6d
Rohini Kar <rohinikar@users.noreply.github.com>
2016-05-20 14:45:26 -0700

    Updated

--- Even Fibonacci Numbers.java
+++ Even Fibonacci Numbers.java
@@ -7,16 +7,15 @@ public static void main(String[] args)
 int fibonacci(int i1, int i2)
 {
     int result = 0;
-	result += i2;
-	    int sum = i1 + i2;
-	while (sum <= 4000000)
-	{
-	    i1 = i2;
-		i2 = sum;
-		sum = i1 + i2;
-		
-		if(sum % 2 == 0)
-		    result += sum;
-	}
-	return result;
+    result += i2;
+    int sum = i1 + i2;
+    while (sum <= 4000000)
+    {
+	i1 = i2;
+        i2 = sum;
+	sum = i1 + i2;
+	if(sum % 2 == 0)
+            result += sum;
+    }
+    return result;
 }
